451bf73d99f7241f9bc3136e542fdcc3de4be1b2
Elsa Bernamont <elsabrnt@gmail.com>
2026-02-13 16:36:09 +0100

    ok

--- components/LastTweet.js
+++ components/LastTweet.js
@@ -1,84 +1,122 @@
-import styles from '../styles/Tweet.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteTweet, likeTweet } from '../reducers/tweets';
-import ReactHashtag from 'react-hashtag';
-import { useRouter } from 'next/router';
-
-
+import styles from "../styles/LastTweet.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTweet, likeTweet } from "../reducers/tweets";
+import ReactHashtag from "react-hashtag";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import moment from "moment";
 
 function LastTweet(props) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+  const router = useRouter();
+  const timeElapsed = moment(props.createdAt).fromNow();
 
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user.value);
-    const router = useRouter();
+  const handleLike = () => {
+    fetch("http://localhost:3000/tweets/like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: user.token,
+        tweetId: props._id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(likeTweet({ tweetId: props._id, username: user.username }));
+        }
+      });
+  };
 
-    const handleLike = () => {
-        fetch('http://localhost:3000/tweets/like', {
-        method: 'POST', 
-        headers: { 
-            'Content-Type': 'application/json' }, 
-            body: JSON.stringify({ 
-                token: user.token, 
-                tweetId: props._id }),
-             }) 
-             .then(response => response.json()) 
-             .then(data => { 
-                if (data.result) 
-                { dispatch(likeTweet({ tweetId: props._id, username: user.username})); 
-                 }
-                     }) 
-    }
+  let heartIconStyle = {};
+  if (props.isLiked) {
+    heartIconStyle = { color: "red", cursor: "pointer" };
+  }
 
+  const handleDelete = () => {
+    fetch("http://localhost:3000/tweets/delete", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: user.token,
+        tweetId: props._id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(deleteTweet({ tweetId: props._id }));
+        }
+      });
+  };
 
-    let heartIconStyle = {};
-	if (props.isLiked) {
-		heartIconStyle = { 'color': 'red', 'cursor': 'pointer' };
-	}
-
-     const handleDelete = () => {
-        fetch('http://localhost:3000/tweets/delete', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'},
-                body: JSON.stringify({ 
-                    token: user.token, 
-                    tweetId: props._id}),
-        }) 
-        .then(response => response.json()) 
-        .then(data => {
-            if (data.result) 
-            {
-             dispatch(deleteTweet({ tweetId: props._id })); 
-            }
-         })}
-
-         return (
-            <div>
-               <p>
-         <strong>{props.author?.username}</strong> : {' '}
+  return (
+    <div className={styles.tweetBox}>
+      <div className={styles.tweetHeader}>
+        <div className={styles.profilePic}>
+          <Image
+            src="/images/egg.jpg"
+            alt="Profile Picture"
+            width={40}
+            height={40}
+            style={{ borderRadius: "40%", margin: "15px" }}
+          />
+        </div>
+        <div className={styles.userInfo}>
+          <span className={styles.username}>{props.author?.username}</span>
+          <span className={styles.handle}>
+            @{props.author?.username} • {timeElapsed}
+          </span>
+        </div>
+      </div>
+      <div className={styles.tweetContent}>
         <ReactHashtag
-         onHashtagClick={(hash) => props.selectHashtag(hash)}
-        renderHashtag={(hashtagValue) => (
-          <span style={{ color: '#1d9bf0', fontWeight: 'bold', cursor: 'pointer' }}>
-          {hashtagValue}
-         </span>
-         )}
+          onHashtagClick={(hash) => props.selectHashtag(hash)}
+          renderHashtag={(hashtagValue) => (
+            <span
+              style={{
+                color: "#1d9bf0",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              {hashtagValue}
+            </span>
+          )}
         >
-         {props.content}
+          {props.content}
         </ReactHashtag>
-        
-          </p>
-             <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLike()} style={heartIconStyle} className="like" /></span>
-             {props.author?.username === user.username && (
-             <span><FontAwesomeIcon icon={faTrashCan} onClick={() => handleDelete()} style={{ cursor: 'pointer', marginLeft: '10px' }} className={styles.deleteBtn}/></span> )}
-            </div>
-         )
-       
+      </div>
+      <div className={styles.actions}>
+        <span>
+          <FontAwesomeIcon
+            icon={faHeart}
+            onClick={() => handleLike()}
+            style={{ ...heartIconStyle, cursor: "pointer" }}
+            className="like"
+          />
+          <span className={styles.likeCount}>{props.likes?.length || 0}</span>
+        </span>
+        {props.author?.username === user.username && (
+          <span>
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              onClick={() => handleDelete()}
+              style={{ cursor: "pointer", marginLeft: "10px" }}
+              className={styles.deleteBtn}
+            />
+          </span>
+        )}
+      </div>
+    </div>
+  );
 }
-
-
-
 
 export default LastTweet;
--- components/Tweet.js
+++ components/Tweet.js
@@ -1,140 +1,146 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { addTweet, displayTweets } from '../reducers/tweets';
-import { useState, useEffect } from 'react';
-import styles from '../styles/Tweet.module.css';
-import LastTweet from './LastTweet';
-import Trends from './Trends';
-import { logout } from '../reducers/user';
-import { useRouter } from 'next/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { useDispatch, useSelector } from "react-redux";
+import { addTweet, displayTweets } from "../reducers/tweets";
+import { useState, useEffect } from "react";
+import styles from "../styles/Tweet.module.css";
+import LastTweet from "./LastTweet";
+import Trends from "./Trends";
+import { logout } from "../reducers/user";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import Image from "next/image";
 
 
-import Link from 'next/link';
-
-
+import Link from "next/link";
 
 function Tweet() {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user.value);
-    const tweets = useSelector((state) => state.tweets.value);
-    const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+  const tweets = useSelector((state) => state.tweets.value);
+  const router = useRouter();
 
-    const [newTweet, setNewTweet] = useState('');
+ 
 
-const handleClick = () => {
+  const [newTweet, setNewTweet] = useState("");
+  
+
+  const handleClick = () => {
     if (!user.token) {
-        alert("Vous devez être connecté pour tweeter !");
-        return;
-    } 
-  fetch('http://localhost:3000/tweets/add', {
-    method: 'POST', 
-    headers: { 
-        'Content-Type': 'application/json' }, 
-        body: JSON.stringify({ token: user.token, content: newTweet }),
-         }) 
-         .then(response => response.json()) 
-         .then(data => { 
-            if (data.result) 
-            { dispatch(addTweet(data.tweet)); 
-                setNewTweet('');
-             } else {
-                 alert('Erreur : ' + data.error); } 
-}) 
-                
- };
+      alert("Vous devez être connecté pour tweeter !");
+      return;
+    }
+    fetch("http://localhost:3000/tweets/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: user.token, content: newTweet }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(addTweet(data.tweet));
+          setNewTweet("");
+        } else {
+          alert("Erreur : " + data.error);
+        }
+      });
+  };
 
- const displayTweet = () => {
-  fetch('http://localhost:3000/tweets')
-         .then(response => response.json()) 
-         .then(data => { 
-            if (data.result) 
-            { dispatch(displayTweets(data.tweets)); 
-             } }) 
-            
- };
+  const displayTweet = () => {
+    fetch("http://localhost:3000/tweets")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(displayTweets(data.tweets));
+        }
+      });
+  };
 
- useEffect(() => {
-  displayTweet();
-}, []);
- 
- 
+  useEffect(() => {
+    displayTweet();
+  }, []);
 
-const handleLogout = () => {
-		dispatch(logout());
-        router.push('/');
-	};
+  const handleLogout = () => {
+    dispatch(logout());
+    router.push("/");
+  };
 
-let userSection;
-	if (user.token) {
-		userSection = (
-			<div className={styles.logoutSection}>
-        <Image className={styles.eggLogo}
+  let userSection;
+  if (user.token) {
+    userSection = (
+      <div className={styles.logoutSection}>
+        <Image
+          className={styles.eggLogo}
           src="/images/egg.jpg"
           alt="Logo"
           width={40}
-          height={40} 
-          >
-        </Image>
-				<p> @{user.username} </p>
-				<button className={styles.btnLogout} onClick={() => handleLogout()}>Logout</button>
-			</div>
-		);
-	}
+          height={40}
+        ></Image>
+        <p> @{user.username} </p>
+        <button className={styles.btnLogout} onClick={() => handleLogout()}>
+          Logout
+        </button>
+      </div>
+    );
+  }
 
   const handleHashtagClick = (hashValue) => {
     const tagName = hashValue.slice(1);
-     router.push(`/hashtag/${tagName}`);
+    router.push(`/hashtag/${tagName}`);
   };
 
   const twitterData = tweets.map((data, i) => {
-  const isLiked = data.likes.some(
-    (like) => like.username === user.username  );
-  return <LastTweet key={data._id} {...data} isLiked={isLiked} selectHashtag={handleHashtagClick}/>;
-});
+    const isLiked = data.likes.some((like) => like.username === user.username);
+    return (
+      <LastTweet
+        key={data._id}
+        {...data}
+        isLiked={isLiked}
+        selectHashtag={handleHashtagClick}
+      />
+    );
+  });
 
-
-
-
- return (
+  return (
     <div>
-       <main className={styles.main}>  
+      <main className={styles.main}>
         <div className={styles.leftSection}>
-          <Link href="/"  >
-          <FontAwesomeIcon className={styles.twitterIcon} icon={faTwitter} flip="vertical"/>
+          <Link href="/">
+            <FontAwesomeIcon
+              className={styles.twitterIcon}
+              icon={faTwitter}
+              flip="vertical"
+            />
           </Link>
-             {userSection}
-            
-
+          {userSection}
         </div>
 
         <div className={styles.middleSection}>
+          <h2 className={styles.h2}>Home</h2>
 
-       
-        <h2 className={styles.h2}>Home</h2>
-        
           <div className={styles.addTweetContainer}>
-           <input type="text" placeholder="What's up ?" className={styles.inputTweet} maxLength={280} onChange={(e) => setNewTweet(e.target.value)} value={newTweet}/>
-           <span className={styles.compteur}>{newTweet.length}/280</span>
-           <button className={styles.btnTweet} onClick={() => handleClick()}>Tweet</button>
-           
+            <input
+              type="text"
+              placeholder="What's up ?"
+              className={styles.inputTweet}
+              maxLength={280}
+              onChange={(e) => setNewTweet(e.target.value)}
+              value={newTweet}
+            />
+            <span className={styles.compteur}>{newTweet.length}/280</span>
+            <button className={styles.btnTweet} onClick={() => handleClick()}>
+              Tweet
+            </button>
           </div>
           <div className={styles.tweetContainer}>{twitterData}</div>
-           <div className={styles.tweetContainer}>
-             
-            </div>
-      </div>
-      <div className={styles.rightSection}>
-        
-        <h2 className={styles.h2}>Trends</h2>
-        
-
-      </div>
+        </div>
+        <div className={styles.rightSection}>
+          <h2 className={styles.h2}>Trends</h2>
+        </div>
       </main>
-   </div>
-      
- );
-  }
+    </div>
+  );
+}
 
 export default Tweet;
